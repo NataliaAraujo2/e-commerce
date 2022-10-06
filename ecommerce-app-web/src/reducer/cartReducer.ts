2fafc9477823d9ce2034b99592464e7ba86accbd
NataliaAraujo2 <102322514+NataliaAraujo2@users.noreply.github.com>
2022-10-06 04:16:30 -0300

    Cria função para ação do carrinho

---
+++ ecommerce-app-web/src/reducer/cartReducer.ts
@@ -0,0 +1,7 @@
+import { CartActionReducer, CartItem } from "../types/typeApp";
+
+const cartReducer = (state: CartItem[] , action: CartActionReducer) => {
+    return state;
+}
+
+export default cartReducer
