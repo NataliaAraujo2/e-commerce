20e409ae3e24ba53e2558efe1aadece83772aa95
NataliaAraujo2 <102322514+NataliaAraujo2@users.noreply.github.com>
2022-10-06 04:15:14 -0300

    Estabelec tipos

---
+++ ecommerce-app-web/src/types/typeApp.ts
@@ -0,0 +1,32 @@
+export type ProductFetch = {
+    products: ProductItem[];
+    isLoading: boolean;
+    isError: boolean;
+}
+
+export type ProductItem = {
+    id: number;
+    category: string;
+    title: string;
+    image: string;
+    description: string;
+    price: number;
+}
+
+export type CartItem = {
+    id?: number;
+    title: string;
+    image?: string;
+    price: number;
+    amount: number;
+}
+
+export type CartActionReducer = {
+    payload: any;
+    type: 'ADD' | 'REMOVE' | 'REMOVE-ALL' | 'CLEAR';
+}
+
+export type CartContextType = {
+    cartItems: CartItem[],
+    dispatch: React.Dispatch<CartActionReducer>
+}
