585909d8b86a416b6e5189d92eb85dae10a3af23
NataliaAraujo2 <Felix@DESKTOP-IF5D9PB>
2022-10-06 13:50:56 -0300

    .

---
+++ ecommerce-app-web/src/components/Cart/Item/Item.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CartItem } from "../../../types/typeApp";
+import { faMinus, faPlus, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { Button } from  "react-bootstrap";
+
+type Props = {
+    item:CartItem
+}
+const Item =()=>{
+    return (
+        <>
+         <div className="shopping-cart-item">
+            <div className="item-action">
+                <Button variant="secondary">
+                    <FontAwesomeIcon icon={faPlus} color="white" />
+                    <FontAwesomeIcon icon={faMinus} color="white" />
+                </Button>
+            </div>
+            <div className="item-detail">
+                <div className="item-detail-image">
+                    <image src={item.image} alt={item.title}/>
+                </div>
+                <div className="item-detail-info">
+                    <div className="item-detail-info-name">
+                        {item.title}
+                    </div>
+                    <div className="item-detail-info-prices">
+                        <span>{item.price}</span>
+                    </div>
+                </div>
+            </div>
+            <div className="item-action-remove">
+                <Button
+                    variant="danger"
+                    Title="Retirar produto do carrinho">
+                    <FontAwesomeIcon icon={faTrashAlt} color="white"     
+                </Button>
+            </div>
+         </div>
+        
+        </>
+
+    )
+}
+
+export default Item;
