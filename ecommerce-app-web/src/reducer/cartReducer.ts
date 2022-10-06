f9614a0b4e19b2fb5b9c696eec557f5d161c042e
NataliaAraujo2 <Felix@DESKTOP-IF5D9PB>
2022-10-06 15:33:50 -0300

    Estabelece case add e case remove

--- ecommerce-app-web/src/reducer/cartReducer.ts
+++ ecommerce-app-web/src/reducer/cartReducer.ts
@@ -1,7 +1,49 @@
+import Item from "../components/Cart/Item/Item";
 import { CartActionReducer, CartItem } from "../types/typeApp";
 
-const cartReducer = (state: CartItem[] , action: CartActionReducer) => {
+const cartReducer = (state: CartItem[], action: CartActionReducer) => {
+
+    switch (action.type) {
+
+        case "ADD":
+            const existProduct  = state.find(item=> item.id === action.payload.id);
+            if(existProduct){
+                return state.map(item =>{
+                    if(item.id === action.payload.id){
+                        return {
+                            ...item,
+                            amount: item.amount + 1
+                        }
+                    }
+                    return item;
+                })
+            } else {
+                const { id, title, image, price } = action.payload;
+                return [...state, { id, title, image, price, amount: 1 }];
+            }
+
+        case "REMOVE":
+            return state.reduce((acum,item) => {
+                if(item.id === action.payload){
+                    if(item.amount===1) return acum
+                    else return [...acum,{...item, amount: item.amount-1}]
+                }
+                return [...acum, item];
+
+            }, [] as CartItem[]);
+
+        case "REMOVE-ALL":
+            return state;
+
+        case "CLEAR":
+            return state;
+
+        default:
+            return state;
+
+    }
+
     return state;
 }
 
-export default cartReducer
+export default cartReducer;
